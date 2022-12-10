7f7d38cc97849e2cbb3f399e7ed18cf9681af82c
Ratandeep <chaddharatandeepsingh2345@gmail.com>
2022-12-10 22:20:16 +0530

    login backend done

--- server/data/users.js
+++ server/data/users.js
@@ -4,18 +4,18 @@ const users = [
 	{
 		name: 'Admin User',
 		email: 'admin@gmail.com',
-		password: bcrypt.hashSync('12345', 10),
+		password: bcrypt.hashSync('123456', 10),
 		isAdmin: true
 	},
 	{
 		name: 'Test User',
 		email: 'test@gmail.com',
-		password: bcrypt.hashSync('12345', 10),
+		password: bcrypt.hashSync('123456', 10),
 	},
 	{
 		name: 'John User',
 		email: 'john@gmail.com',
-		password: bcrypt.hashSync('12345', 10),
+		password: bcrypt.hashSync('123456', 10),
 	},
 ]
 
